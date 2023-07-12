aabecbd9f9b218318909976c81447ba722595613
Radhikatag <radhikadev816@gmail.com>
2023-07-12 10:47:02 +0000

    terraform code commit

---
+++ terraform_provider.tf
@@ -0,0 +1,26 @@
+#Terraform settings block
+terraform {
+  required_version = "~> 1.4"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 4.0"
+
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+  access_key = "AKIA4IPT2Y34776VAAND"
+  secret_key = "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"
+}
+
+resource "aws_instance" "ec2demo" {
+  ami       =  "ami-06b79cf2aee0d5c92"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "maven_node"
+  }
+}
