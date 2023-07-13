8ffeec403e22ed7027767f3ee1a6435bc16f702a
Radhikatag <131761310+Radhikatag@users.noreply.github.com>
2023-07-13 14:39:49 +0530

    Update tool_install.sh
    newly add ansible

--- tool_install.sh
+++ tool_install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 #install git 
 #Install terraform
+#Install ansible
 #This is aws linux machine
 
 $yum install git -y
 
 $yum update -y
+$sudo amazon-linux-extras install epel -y
+$sudo yum-config-manager --enable epel
+$sudo yum repolist
+$sudo yum install ansible
 $sudo yum install -y yum-utils
 $sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 $sudo yum -y install terraform
