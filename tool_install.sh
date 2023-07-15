d7ab6122290e9302e46260505dbdcb652dba9642
Radhikatag <radhikadev816@gmail.com>
2023-07-15 08:01:03 +0000

    commit message

--- tool_install.sh
+++ tool_install.sh
@@ -6,16 +6,12 @@
 
 $yum install git -y
 
-$yum update -y
+
 $sudo amazon-linux-extras install epel -y
 $sudo yum-config-manager --enable epel
 $sudo yum repolist
-$sudo yum install ansible
+$sudo yum install ansible -y
 $sudo yum install -y yum-utils
 $sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 $sudo yum -y install terraform
 $yum install docker -y
-$usermod -a -G docker ec2-user
-$systemctl enable docker
-$systemctl start docker
-
